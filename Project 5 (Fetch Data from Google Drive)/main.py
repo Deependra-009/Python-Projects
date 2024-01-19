d51980cb013467b59a322b447eb7e2169fad3020
Deependra-009 <deependra04trivedi@gmail.com>
2024-01-20 01:56:01 +0530

    update

--- Project 5 (Fetch Data from Google Drive)/main.py
+++ Project 5 (Fetch Data from Google Drive)/main.py
@@ -4,7 +4,7 @@ import json
 
 # Provide your credentials (client ID, client secret, etc.) here
 credentials = Credentials(
-    "TOKEN",
+    token="",
     refresh_token="",
     token_uri="",
     client_id="",
@@ -15,43 +15,56 @@ credentials = Credentials(
 service = build('drive', 'v3', credentials=credentials)
 
 # ID of the folder you want to access (replace with your folder ID)
-folder_id = '1BhuncRjufS7Y0FEI33Tv_jpTmYFs6kv8'
+folder_id = '19-nJUHhZiQAzAvl4T9D9l3doqQbGSIRC'
 # Call the Drive v3 API
-results = service.files().list(q=f"'{folder_id}' in parents",pageSize=18).execute()
+results = service.files().list(q=f"'{folder_id}' in parents",pageSize=50).execute()
 # get the results
 items = results.get('files', [])
 
 url="https://drive.google.com/uc?export=view&id="
 
 dict={}
-
+list=[]
 for i1 in items:
     r1 = service.files().list(q=f"'{i1['id']}' in parents", pageSize=18).execute()
+    # print(i1)
 
-    for i2 in r1.get('files',[]):
+    # <img src="https://drive.google.com/uc?export=view&id=1N-msX_JEDVI6d2zTJLROCFxXtHn270m4">
+    name=i1['name'].split('.')[0]
+    url="<img src='https://drive.google.com/uc?export=view&id="+i1['id']+"'>"
+    print(url+" "+name)
+    dict[int(name)]=url
+    list.append(url)
 
-        print(i1['name']+" "+i2['name'])
+    # for i2 in r1.get('files',[]):
 
-        r2 = service.files().list(q=f"'{i2['id']}' in parents", pageSize=18).execute()
+        # print(i1['name']+" "+i2['name'])
+        #
+        # r2 = service.files().list(q=f"'{i2['id']}' in parents", pageSize=18).execute()
+        #
+        # list=[]
+        #
+        # for i3 in r2.get('files',[]):
+        #     final_url=url+i3['id']
+        #     list.append(final_url)
+        #     print(final_url)
+        #
+        # dict[i2['name']]=list
 
-        list=[]
-
-        for i3 in r2.get('files',[]):
-            final_url=url+i3['id']
-            list.append(final_url)
-            print(final_url)
-
-        dict[i2['name']]=list
-
-    print()
+    # print()
 
 
 print(dict)
-
+print(list.reverse())
+list.reverse()
 file_path = 'home&living.json'  # Replace 'mendata.json' with your desired file name and path
 
 # Write the dictionary content to a JSON file
-with open(file_path, 'w') as json_file:
-    json.dump(dict, json_file)
+with open(file_path, 'w') as file:
+    for i in range(1,30,1):
+        print(i)
+        str=dict[i]
+        file.write('\n'+str)
+
 
 
---
+++ Project 6 (OpenAI PDF Generate)/index.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+from reportlab.pdfgen import canvas
+import os
+
+# Set your OpenAI API key from the environment variable
+
+client = OpenAI(
+    api_key="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"
+)
+
+
+# Function to generate responses and create a PDF file
+def generate_and_save_to_pdf(prompt):
+    # Make API request to GPT-3.5
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    print(completion.choices[0].message.content)
+    # Extract the generated text from the API response
+    generated_text = completion.choices[0].message.content
+
+    lines=generated_text.split("/n")
+
+    # # Create a PDF file with the generated text
+    with open('textfile.txt', 'w') as file:
+        for line in lines:
+            file.write(line)
+
+
+# Example usage
+
+
+# Sample prompt
+prompt = "write a story of 100 words of any topic'"
+
+generate_and_save_to_pdf(prompt)
