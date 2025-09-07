2e0915c6d9336a9fb08c2a8a8ad1a52392594efe
Deependra-009 <deependra04trivedi@gmail.com>
2025-09-07 11:48:19 +0530

    add new project

--- Project 9 (Click Mouse cursor)/main.py
+++ Project 9 (Click Mouse cursor)/main.py
@@ -1,31 +1,44 @@
 import pyautogui
 import time
-
-# Give yourself a few seconds to move to the right window
 time.sleep(3)
 
-# select draft
-pyautogui.moveTo(750, 350, duration=1)
-pyautogui.click()
+def sendMail():
+    # Give yourself a few seconds to move to the right window
+    time.sleep(1)
 
-# Click at that position
-# pyautogui.click()
-# send immediately
-# pyautogui.moveTo(1300,1050,duration=1)
+    # select draft
+    pyautogui.moveTo(750, 350, duration=1)
+    pyautogui.click()
 
-# send schedule
-pyautogui.moveTo(1350,1050,duration=1)
-pyautogui.click()
+    # send immediately
+    pyautogui.moveTo(1300,1050,duration=1)
+    pyautogui.click()
 
-# select schedule
-pyautogui.moveTo(1350,1000,duration=1)
-pyautogui.click()
-
-# select time
-pyautogui.moveTo(1000,600,duration=1)
-pyautogui.click()
-
-print(pyautogui.position())
+    # sendBySchedule();
 
 
-pyautogui.FAILSAFE = True
+
+    print(pyautogui.position())
+
+
+    pyautogui.FAILSAFE = True
+
+
+def sendBySchedule():
+    # send schedule
+    pyautogui.moveTo(1350, 1050, duration=1)
+    pyautogui.click()
+
+    # select schedule
+    pyautogui.moveTo(1350, 1000, duration=1)
+    pyautogui.click()
+
+    # select time
+    pyautogui.moveTo(1000, 600, duration=1)
+    pyautogui.click()
+
+
+
+
+for i in range(1,5):
+    sendMail()
